61d7206642221dd20e29d13472413949dfb5b6c3
FrancoF2002 <elpanqui44@gmail.com>
2021-12-22 17:47:16 -0300

    Finalizacion de Js.

--- js/index.js
+++ js/index.js
@@ -1,17 +1,62 @@
 const opcionesCriptomoneda = async () => {
-    debugger;
-    const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD";
+    const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=100&tsym=USD";
 
     const respuesta = await fetch(url)
     const resultado = await respuesta.json();
 
     // console.log(resultado)
-    let select = document.querySelector("#Criptomoneda")
-    let opcionesHTML = `<option value=""> -Selecciona- </option>`;
+    let selectCripto = document.querySelector("#criptomoneda")
+    let opcionesHTML = `<option disabled selected value="">- Seleccione -</option>`;
     
     resultado.Data.map(opcion =>{
         opcionesHTML += `<option value="${opcion.CoinInfo.Name}">${opcion.CoinInfo.FullName}</option>`;
-    } );
-    let selectCripto = ""
-    selectCripto.innertHTML = opcionesHTML;
+    });
+    selectCripto.innerHTML = opcionesHTML;
 }
+
+const cotizarMoneda = () => {
+
+    let divError = document.getElementById("msj-error")
+    const moneda = document.querySelector("#moneda").value;
+    const cripto = document.querySelector("#criptomoneda").value;
+
+    //Encaso de agregar un select activar validacion.
+
+    // if (moneda === '' || cripto === '') {
+    //     divError.style.display = 'block';
+    //     return;
+    // } else{
+    //     divError.style.display = 'none';
+    // }
+
+
+    cotizar(moneda, cripto)
+}
+
+const cotizar = async(moneda = "USD", cripto="BTC") => {
+    
+    const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${cripto}&tsyms=${moneda}`
+    //console.log(url)
+    const respuesta = await fetch(url)
+    let resultado = await respuesta.json();
+
+    //console.log(resultado.DISPLAY[cripto][moneda])
+
+    resultado = resultado.DISPLAY[cripto][moneda];
+    let divResultado = document.querySelector("#divResultado")
+    let divAlto = document.querySelector("#divMasAlto")
+    let divBajo = document.querySelector("#divMasBajo")
+    let divVar = document.querySelector("#divVariacion")
+    let divAct = document.querySelector("#divActualizacion")
+
+
+
+        divResultado.innerHTML = "El precio es " + resultado.PRICE
+        divAlto.innerHTML = "El precio mas alto del dia es: " + resultado.HIGHDAY
+        divBajo.innerHTML = "El precio mas bajo del dia: " + resultado.LOWDAY
+        divVar.innerHTML = "Variacion Ultimas 24hs: " + resultado.CHANGEPCT24HOUR
+        divAct.innerHTML = "Ultima actualizacion: " + resultado.LASTUPDATE
+
+}
+
+
